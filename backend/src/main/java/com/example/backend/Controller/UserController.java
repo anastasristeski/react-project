87005b91c92e07052c42d9fe5c9323a43db517fd
anastasristeski <anastas.risteski5@gmail.com>
2025-08-13 13:31:47 +0200

    finalcommit2

--- backend/src/main/java/com/example/backend/Controller/UserController.java
+++ backend/src/main/java/com/example/backend/Controller/UserController.java
@@ -11,21 +11,17 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
     private final UserRepository userRepository;
-    private final JwtUtil jwtUtil;
     private final AuthenticationService service;
 
-    public UserController(UserRepository userRepository, JwtUtil jwtUtil, AuthenticationService service) {
+    public UserController(UserRepository userRepository, AuthenticationService service) {
         this.userRepository = userRepository;
-        this.jwtUtil = jwtUtil;
         this.service = service;
     }
     @GetMapping
